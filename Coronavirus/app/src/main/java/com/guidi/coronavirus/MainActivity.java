26bc8f48753093dd93b11b84a9c4dafdafdefb3f
Khiem Phi <khiemais@gmail.com>
2020-06-27 17:31:21 -0400

    Added Login Screen and Started Authentication

--- Coronavirus/app/src/main/java/com/guidi/coronavirus/MainActivity.java
+++ Coronavirus/app/src/main/java/com/guidi/coronavirus/MainActivity.java
@@ -1,5 +1,6 @@
 package com.guidi.coronavirus;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -9,17 +10,23 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int SIGN_UP_ACTIVITY_CODE = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+        startActivityForResult(intent, SIGN_UP_ACTIVITY_CODE);
+
+
         setContentView(R.layout.activity_main);
+        // Creating Bottom Navigator View
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
---
+++ Coronavirus/app/src/main/java/com/guidi/coronavirus/SignUpActivity.java
@@ -0,0 +1,29 @@
+package com.guidi.coronavirus;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser != null){
+            // Return to MainActivity With User-ID
+        }
+
+    }
+
+    
+}
