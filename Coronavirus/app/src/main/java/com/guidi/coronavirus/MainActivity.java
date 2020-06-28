587b9c369f6f00d0082a0b7df8a1f0aab5c6756a
Khiem Phi <khiemais@gmail.com>
2020-06-27 21:42:40 -0400

    Added SignIn, Bug at SignIn

--- Coronavirus/app/src/main/java/com/guidi/coronavirus/MainActivity.java
+++ Coronavirus/app/src/main/java/com/guidi/coronavirus/MainActivity.java
@@ -10,33 +10,39 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int SIGN_UP_ACTIVITY_CODE = 123;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
-        startActivityForResult(intent, SIGN_UP_ACTIVITY_CODE);
-
-
-        setContentView(R.layout.activity_main);
-        // Creating Bottom Navigator View
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_planner, R.id.navigation_fun, R.id.navigation_achievements, R.id.navigation_settings)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
-
+        mAuth = FirebaseAuth.getInstance();
+        String currentUserID = mAuth.getCurrentUser().getUid();
+        System.out.println(currentUserID);
+        if (currentUserID != null) {
+            // Have currentUserID here!
+            setContentView(R.layout.activity_main);
+            // Creating Bottom Navigator View
+            BottomNavigationView navView = findViewById(R.id.nav_view);
+            // Passing each menu ID as a set of Ids because each
+            // menu should be considered as top level destinations.
+            AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                    R.id.navigation_planner, R.id.navigation_fun, R.id.navigation_achievements, R.id.navigation_settings)
+                    .build();
+            NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+            NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+            NavigationUI.setupWithNavController(navView, navController);
+        }else{
+            Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+            startActivityForResult(intent, SIGN_UP_ACTIVITY_CODE);
+        }
     }
-
 }
--- Coronavirus/app/src/main/java/com/guidi/coronavirus/SignUpActivity.java
+++ Coronavirus/app/src/main/java/com/guidi/coronavirus/SignUpActivity.java
@@ -1,16 +1,31 @@
 package com.guidi.coronavirus;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.text.TextUtils;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private Button signUpButton;
+    private Button loginButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,11 +34,46 @@ public class SignUpActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
 
+        initSignUpButton();
         if (currentUser != null){
             // Return to MainActivity With User-ID
         }
 
     }
 
-    
+    private void initSignUpButton(){
+        signUpButton = findViewById(R.id.signup_btn);
+        emailEditText = findViewById(R.id.email_edt_text);
+        passwordEditText = findViewById(R.id.pass_edt_text);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)){
+                    Toast.makeText(SignUpActivity.this, "Please Fill In The Empty Fields", Toast.LENGTH_LONG).show();
+                }else{
+                    mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(SignUpActivity.this, "Successfully Registered", Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                                String currentUserID = mAuth.getCurrentUser().getUid();
+                                intent.putExtra("userID", currentUserID);
+                                setResult(RESULT_OK, intent);
+                                startActivity(intent);
+                                finish();
+                            }else{
+                                Toast.makeText(SignUpActivity.this, "Registration Failed", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+
 }
